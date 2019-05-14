0c07e835dccffd52658ac7e5d0479ce74c85e807
yaser <yaser.darzi@gmail.com>
2019-05-14 15:46:21 +0430

    fix app

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'test'),
+    'secret' => env('JWT_SECRET', 'limitlessco.ir'),
 
     /*
     |--------------------------------------------------------------------------
