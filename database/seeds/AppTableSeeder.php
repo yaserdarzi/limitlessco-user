0d6c338f98d7e8d0bbff50d3fd9b7ab57f0a1c8d
yaser <yaser.darzi@gmail.com>
2019-05-14 16:04:06 +0430

    fix app

--- database/seeds/AppTableSeeder.php
+++ database/seeds/AppTableSeeder.php
@@ -39,7 +39,38 @@ class AppTableSeeder extends Seeder
             "type_app_child" => $app->type_app_child,
         );
         $appSecret = JWT::encode($object, config("jwt.secret"));
-        echo $appSecret . "\n";
+        echo "justkish= " . $appSecret . "\n";
+
+
+        $app = \App\App::create([
+            'app' => 'hotel',
+            'type_app' => 'hotel',
+            'type_app_child' => '',
+            'cash_back' => 0,
+            'info' => ''
+        ]);
+        $info_payment = ['marchedcode' => 'q223qwecasddas'];
+        $info_sms = [
+            'kavenegar_api_key' => $app->type_app,
+            'otp_template' => $app->type_app
+        ];
+        \App\AppSetting::create([
+            'app_id' => $app->id,
+            'type_payment' => \App\Inside\Constants::MARKET_ZARINPAL,
+            'info_payment' => (object)$info_payment,
+            'type_sms' => \App\Inside\Constants::SMS_KAVENEGAR,
+            'info_sms' => (object)$info_sms,
+            'info' => ''
+        ]);
+        $object = array(
+            "app_id" => $app->id,
+            "app" => $app->app,
+            "type_app" => $app->type_app,
+            "type_app_child" => $app->type_app_child,
+        );
+        $appSecret = JWT::encode($object, config("jwt.secret"));
+        echo "hotel= " . $appSecret . "\n";
+
         $app = \App\App::create([
             'app' => 'hotel',
             'type_app' => 'hotel',
@@ -67,7 +98,7 @@ class AppTableSeeder extends Seeder
             "type_app_child" => $app->type_app_child,
         );
         $appSecret = JWT::encode($object, config("jwt.secret"));
-        echo $appSecret . "\n";
+        echo "hotel-sunrise= " . $appSecret . "\n";
 
     }
 }
