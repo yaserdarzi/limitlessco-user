d110c825c911dd6991d024c15f5a5187ef437681
yaser <yaser.darzi@gmail.com>
2019-05-27 18:29:49 +0430

    fix ticket

---
+++ app/Http/Controllers/Api/V1/CP/Agency/TicketController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\CP\Agency;
+
+use App\Exceptions\ApiException;
+use App\Http\Controllers\ApiController;
+use App\Inside\Constants;
+use App\Shopping;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class TicketController extends ApiController
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $customer_id = Constants::SALES_TYPE_AGENCY . "-" . $request->input('agency_id') . "-";
+        $skip = 0;
+        if ($request->input('page') != null)
+            if ($request->input('page') != 0)
+                $skip = 10 * $request->input('page');
+        $shopping = Shopping::where('customer_id', 'like', "%{$customer_id}%");
+        if ($request->input('search')) {
+            $search = $request->input('search');
+            $shopping = $shopping
+                ->where('name', 'LIKE', "%$search%")
+                ->orWhere('phone', 'LIKE', "%$search%");
+        }
+        $shopping = $shopping->take(10)->skip($skip)
+            ->orderBy('created_at', 'desc')->get();
+        return $this->respond($shopping);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id, Request $request)
+    {
+        //
+    }
+
+    ///////////////////public function///////////////////////
+
+
+}
