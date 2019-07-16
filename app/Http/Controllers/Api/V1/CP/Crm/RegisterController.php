5f67df46df2acfa48f66afabeea6a73226aaa10a
yaser <yaser.darzi@gmail.com>
2019-07-16 17:58:36 +0430

    fix crm register last login

--- app/Http/Controllers/Api/V1/CP/Crm/RegisterController.php
+++ app/Http/Controllers/Api/V1/CP/Crm/RegisterController.php
@@ -189,11 +189,15 @@ class RegisterController extends ApiController
         foreach ($product as $productVal) {
             $shopping_id = Constants::APP_NAME_ENTERTAINMENT . "-" . $productVal->id;
             if (!Commission::where(['customer_id' => $customer_id, 'shopping_id' => $shopping_id])->exists()) {
+                if ($productVal->id == 1)
+                    $percent = Constants::AGENCY_PERCENT_DEFAULT;
+                else
+                    $percent = 30;
                 Commission::create([
                     'customer_id' => $customer_id,
                     'shopping_id' => $shopping_id,
                     'type' => $agency->type,
-                    'percent' => Constants::AGENCY_PERCENT_DEFAULT,
+                    'percent' => $percent,
                 ]);
             }
         }
